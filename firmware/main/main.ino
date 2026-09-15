a08400b6e8ed4cf05409ef36602215d2b91527e4
Abu Bakker Siddiq Siam <siamqwert.2005@gmail.com>
2026-09-15 11:06:29 +0600

    Add ESP32 firmware entry point

---
+++ firmware/main/main.ino
@@ -0,0 +1,14 @@
+/*
+ * High-Speed Line Following Robot
+ * ESP32 Firmware
+ *
+ * Main firmware entry point.
+ */
+
+void setup() {
+    // Hardware initialization will be added here.
+}
+
+void loop() {
+    // Main control loop will be added here.
+}
