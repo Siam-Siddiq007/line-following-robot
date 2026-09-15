78f4ebedefa95317ae1937408de4252b1cc0f111
Abu Bakker Siddiq Siam <siamqwert.2005@gmail.com>
2026-09-15 11:07:19 +0600

    Add hardware pin configuration

---
+++ firmware/main/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Motor driver
+#define PWMA 25
+#define AIN1 26
+#define AIN2 27
+
+#define PWMB 14
+#define BIN1 12
+#define BIN2 13
+
+#define STBY 33
+
+// Sensor pins
+// Final pin assignments will be added after hardware wiring.
+
+#endif
